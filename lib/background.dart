61eb6f0e468eca488259a17d1d1578705c92bd0c
micnngo <micnngo@umich.edu>
2019-11-09 23:29:32 -0800

    ranger and user memes

--- lib/background.dart
+++ lib/background.dart
@@ -12,3 +12,29 @@ class BackGround extends StatelessWidget {
                     fit: BoxFit.fill))));
   }
 }
+
+class RangerLogin extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('images/ranger_meme.png'),
+                    fit: BoxFit.fill))));
+  }
+}
+
+
+class UserLogin extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('images/user_meme.png'),
+                    fit: BoxFit.fill))));
+  }
+}
+
