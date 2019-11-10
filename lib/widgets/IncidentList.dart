e6d41dfbbec417978e701c81bd2af65ecc69beb9
Luis, Andre <Andre_Luis@intuit.com>
2019-11-09 22:44:11 -0800

    updating the Report History page

--- lib/widgets/IncidentList.dart
+++ lib/widgets/IncidentList.dart
@@ -3,25 +3,32 @@ import 'package:flutter/foundation.dart';
 
 
 class ListItem extends StatelessWidget {
-  var items = [{"date": "1", "type": "bad"}, {"date": "3", "type": "good"}];
+  var items = [
+    {"date": "21 Jan 2019", "type": "Poaching"},
+    {"date": "04 Feb 2019", "type": "Dead Animal"},
+    {"date": "01 Mar 2019", "type": "Poaching"},
+    {"date": "11 Mar 2019", "type": "Illegal Good Sold"},
+    {"date": "30 Apr 2019", "type": "Dead Animal"},
+    {"date": "04 May 2019", "type": "Trafficking"}
+
+  ];
 
   Widget _buildProductItem(BuildContext context, int index) {
-    print("INDEX");
-    print(index);
-    return Card(
-      child: Row(
-        children: <Widget>[
-          Text(items[index]["date"]),
-          Text(items[index]["type"])
-        ],
-      ),
-    );
+    return Container(
+        child: new Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Text(items[index]["date"] + ":",
+              style: TextStyle(color: Colors.yellow)),
+            Text(items[index]["type"],
+              style: TextStyle(color: Colors.white)),
+          ],
+        )
+      );
   }
 
   @override
   Widget build(BuildContext context) {
-    print("ITEM LENGTH");
-    print(this.items.length);
     return ListView.builder(
       scrollDirection: Axis.vertical,
       shrinkWrap: true,
