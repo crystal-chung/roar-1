14401a9ac109ecbceae9ab79b6d29820e432ef14
Crystal Chung <crystal_chung@intuit.com>
2019-11-09 19:46:26 -0800

    adding google map and markers

--- lib/authority.dart
+++ lib/authority.dart
@@ -1,20 +1,72 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class AuthorityRoute extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class AuthorityRoute extends StatefulWidget {
+  @override
+  _MapState createState() => _MapState();
+}
+
+
+class _MapState extends State<AuthorityRoute> {
+  GoogleMapController mapController;
+  List<Marker> allMarkers = [];
+
+  final LatLng _center = const LatLng(47.10110364304358, -102.3323937129871);
+
+  @override
+  void initState() {
+    super.initState();
+    final Marker marker = Marker(
+      markerId: MarkerId('firstMarker'),
+      draggable: false,
+      onTap: () {
+        print("hello world");
+      },
+      position: LatLng(47.10110364304358, -102.3323937129871),
+      infoWindow: InfoWindow(
+        title: "Title",
+        snippet: "Content",
+      ),
+    );
+
+    print("printing marker");
+    print(marker);
+    allMarkers.add(marker);
+  }
+
+
+  void _onMapCreated(GoogleMapController controller) {
+
+    setState(() {
+      mapController = controller;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Authority, bitches."),
+        title: Text("Tip Metrics"),
       ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('Home'),
-        ),
-      ),
+        body: Column(
+          children: <Widget> [
+            Container(
+              child: SizedBox(
+                height: 200.0,
+                child: GoogleMap(
+                  onMapCreated: _onMapCreated,
+                  initialCameraPosition: CameraPosition(
+                    target: LatLng(47.10110364304358, -102.3323937129871),
+                    zoom: 11.0,
+                  ),
+                  markers: Set.from(allMarkers),
+                )
+              )
+            )
+          ]
+        )
     );
   }
 }
